dd1e1901c6654134cad1f7ea3d960bfdcbe1c034
Kevin W <kevinwestnyc@gmail.com>
2021-06-18 12:06:58 -0400

    delete button works now

--- main.js
+++ main.js
@@ -3,15 +3,17 @@ const taskInput = document.getElementById('task-input');
 const task = document.getElementsByClassName('task')
 const taskList = document.getElementById('task-list');
 
-// Form submit event
-form.addEventListener('submit', addItem);
+form.addEventListener('submit',addItem);
+form.addEventListener('submit', clearFields);
+taskList.addEventListener('click', removeItem);
 
-
-taskInput.addEventListener('click', removeItem);
+function clearFields() {
+    taskInput.value = "";
+}
 
 function addItem(e){ 
     e.preventDefault();
-    
+        
     const newTask = document.getElementById('task-input').value;
     const createNewTask = document.createElement('li');
     document.querySelector("#task-list").appendChild(createNewTask);
@@ -22,15 +24,25 @@ function addItem(e){
     deleteButton.className = 'delete-button';
     deleteButton.appendChild(document.createTextNode('X'));
     createNewTask.appendChild(deleteButton); 
+    
+    const checkBox = document.createElement("input");
+    checkBox.type = 'checkbox';
+    checkBox.className = 'check-box'
+    createNewTask.appendChild(checkBox);
 }
 
 function removeItem(e){
-    if(e.target.class = delete-button ){
-        const taskToRemove = e.target.parentElement;
-        taskList.removeChild(taskToRemove);
-      }
-    
+    if(e.target.classList.contains('delete-button')){
+    const taskToRemove = e.target.parentElement;
+    taskList.removeChild(taskToRemove);
+    }
+}
+
+function strikeThrough(e){
+    if(e.target.classList.contains('check-box')){
+        const taskToStrikethrough = e.target.parentElement;
+        taskToStrikethrough.style.textDecoration = "line-through";
+        }
 }
 
 //function check-button that adds strike-through
-//function button to delete item
