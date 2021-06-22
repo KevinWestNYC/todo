ccbfa0b66d66570771d692b42cbae283dc2619b5
Kevin W <kevinwestnyc@gmail.com>
2021-06-22 14:45:28 -0400

    checkbutton fixed

--- main.js
+++ main.js
@@ -6,41 +6,16 @@ document.getElementById('task-list').addEventListener('click', removeItem);
 
 function addItem(e){ 
     e.preventDefault();
-
+    const text = document.getElementById('task-input').value;
     const taskList = document.querySelector("#task-list");
     const checkBox = createCheckBox();
     const task = createTask();
     const deleteButton = createDeleteButton(); 
     
     task.appendChild(checkBox);
+    task.append(text);
     taskList.appendChild(task);
     task.appendChild(deleteButton);  
-}
-
-function createDeleteButton() {
-    const deleteButton = document.createElement('button');
-    deleteButton.className = 'delete-button';
-    deleteButton.appendChild(document.createTextNode('X'));
-    return deleteButton;
-}
-
-function createTask() {
-    const text = document.getElementById('task-input').value;
-    const taskLi = document.createElement('li');
-    taskLi.appendChild(document.createTextNode(text));
-    taskLi.className = 'task';
-    return taskLi;
-}
-
-function clearFields() {
-    document.getElementById('task-input').value = "";
-}
-
-function removeItem(e){
-    if(e.target.classList.contains('delete-button')){
-    const taskToRemove = e.target.parentElement;
-    taskToRemove.remove();
-    }
 }
 
 function createCheckBox(){
@@ -53,4 +28,28 @@ function createCheckBox(){
         taskLi.classList.toggle('done')
     })
     return checkBox;
+}
+
+function createTask() {
+    const taskLi = document.createElement('li');
+    taskLi.className = 'task';
+    return taskLi;
+}
+
+function createDeleteButton() {
+    const deleteButton = document.createElement('button');
+    deleteButton.className = 'delete-button';
+    deleteButton.appendChild(document.createTextNode('X'));
+    return deleteButton;
+}
+
+function clearFields() {
+    document.getElementById('task-input').value = "";
+}
+
+function removeItem(e){
+    if(e.target.classList.contains('delete-button')){
+    const taskToRemove = e.target.parentElement;
+    taskToRemove.remove();
+    }
 }
